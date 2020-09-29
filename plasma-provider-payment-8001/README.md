b6ecadf8af3b1f1189e9940fe32c7487931fd8a4
haozai <376419735@qq.com>
2020-09-30 00:05:45 +0800

    添加moudle

---
+++ plasma-provider-payment-8001/README.md
@@ -0,0 +1,6 @@
+plasma-provider-payment-8001
+
+订单创建服务-服务提供者
+
+端口号：8001
+
---
+++ plasma-provider-payment-8001/src/main/java/com/haozai/plasma/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.haozai.plasma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haozai
+ * 程序主启动类
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
